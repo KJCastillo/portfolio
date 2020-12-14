57f49119c5e4cccbf07b6ead29550b25f7f4b363
Kevin Castillo <kevincastillo318@gmail.com>
2020-12-14 17:22:20 -0500

    skills component modified

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,31 +8,31 @@ class Skills extends Component {
         <div className="frontendSkills">
           <h1 className="titleSkills">Frontend</h1>
           <ul>
-            <li><img src="https://img.icons8.com/color/48/000000/html-5.png"/>HTML 5</li>
-            <li><img src="https://img.icons8.com/color/48/000000/css3.png"/>CSS</li>
-            <li><img src="https://img.icons8.com/color/48/000000/javascript.png"/>JavaScript</li>
-            <li><img src="https://img.icons8.com/color/48/000000/bootstrap.png"/>Bootstrap</li>
-            <li><img src="https://img.icons8.com/ios-filled/50/000000/jquery.png"/>jQuery</li>
-            <li><img src="https://img.icons8.com/color/48/000000/react-native.png"/>React.js</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/html-5.png"/>HTML 5</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/css3.png"/>CSS</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/javascript.png"/>JavaScript</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/bootstrap.png"/>Bootstrap</li>
+            <li><img className="icons" src="https://img.icons8.com/ios-filled/50/000000/jquery.png"/>jQuery</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/react-native.png"/>React.js</li>
           </ul>
         </div>
         <div className="backendSkills">
           <h1 className="titleSkills">Backend</h1>
           <ul>
-            <li><img src="https://img.icons8.com/plumpy/48/000000/test-tube.png"/>Flask</li>
-            <li><img src="https://img.icons8.com/color/48/000000/python.png"/>Python</li>
-            <li><img src="https://img.icons8.com/color/48/000000/sql.png"/>SQL</li>
-            <li><img src="https://img.icons8.com/color/48/000000/api.png"/>REST API</li>
+            <li><img className="icons" src="https://img.icons8.com/plumpy/48/000000/test-tube.png"/>Flask</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/python.png"/>Python</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/sql.png"/>SQL</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/api.png"/>REST API</li>
           </ul>
         </div>
         <div className="otherSkills">
           <h1 className="titleSkills">Other</h1>
           <ul>
-            <li><img src="https://img.icons8.com/color/48/000000/git.png"/>Git</li>
-            <li><img src="https://img.icons8.com/fluent/48/000000/github.png"/>Github</li>
-            <li><img src="https://img.icons8.com/color/48/000000/heroku.png"/>Heroku</li>
-            <li><img src="https://img.icons8.com/fluent/48/000000/adobe-photoshop.png"/>Adobe Photoshop</li>
-            <li><img src="https://img.icons8.com/color/48/000000/adobe-illustrator.png"/>Adobe Illustrator</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/git.png"/>Git</li>
+            <li><img className="icons" src="https://img.icons8.com/fluent/48/000000/github.png"/>Github</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/heroku.png"/>Heroku</li>
+            <li><img className="icons" src="https://img.icons8.com/fluent/48/000000/adobe-photoshop.png"/>Adobe Photoshop</li>
+            <li><img className="icons" src="https://img.icons8.com/color/48/000000/adobe-illustrator.png"/>Adobe Illustrator</li>
           </ul>
         </div>
       </div>

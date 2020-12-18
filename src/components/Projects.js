6c0d1ba4205f358d0fd8fcd2f2fce1a479501cd8
Kevin Castillo <kevincastillo318@gmail.com>
2020-12-17 19:35:14 -0500

    started second carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,8 +16,84 @@ class Projects extends Component {
         <div className="projectTitle">
           <h1>Projects</h1>
         </div>
-        <div className="row">
-          {/* START OF MIAMI TECH */}
+
+        <div className='firstProject'>
+        <Carousel className="projectOne" interval={null}>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={MiamiTechHome}
+                style={{ width: "500px", height: "500px" }}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={MiamiTech}
+                style={{ width: "500px", height: "500px" }}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={MiamiTechJobs}
+                style={{ width: "500px", height: "500px" }}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+          <div className="projectOneInfo">
+            <h1>Miami Tech Works</h1>
+            <p>A portal created by the coalition of Miami's business, education and government leaders combining resources to help local citizens. Volunteered to design Jobs Section for desktop and mobile view using Gatsby.js</p>
+          </div>
+          </div>
+
+          <div className="secondProject">
+          <div className="projectTwoInfo">
+            <h1>Liberty Mall</h1>
+            <p>A portal created by the coalition of Miami's business, education and government leaders combining resources to help local citizens. Volunteered to design Jobs Section for desktop and mobile view using Gatsby.js</p>
+          </div>
+          <Carousel className="projectTwo" interval={null}>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={libertyMallSearch}
+                style={{ width: "500px", height: "500px" }}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={loginModal}
+                style={{ width: "500px", height: "500px" }}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={libertyMallRegister}
+                style={{ width: "500px", height: "500px" }}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={cart}
+                style={{ width: "500px", height: "500px" }}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+          </div>
+        
+        
+        {/* <div className="row">
+          START OF MIAMI TECH
           <div className="column">
           <Carousel className="projectOne" interval={null}>
             <Carousel.Item>
@@ -49,10 +125,8 @@ class Projects extends Component {
           <h1>Miami Tech Works</h1>
           <p>A portal created by the coalition of Miami's business, education and government leaders combining resources to help local citizens. Volunteered to design Jobs Section for desktop and mobile view using Gatsby.js</p>
           </div>
-          </div>
-
-            
-          {/* START OF LIBERTY MALL */}
+          </div> 
+          START OF LIBERTY MALL
           <div className="column">
           <Carousel className="projectTwo" interval={null}>
             <Carousel.Item>
@@ -90,10 +164,7 @@ class Projects extends Component {
           </Carousel>
           <p>test test test</p>
               </div>
-
-
-            
-          {/* START OF MOVIESAPI*/}
+          START OF MOVIESAPI
           <div className="column">
           <Carousel className="projectThree" interval={null}>
             <Carousel.Item>
@@ -123,7 +194,7 @@ class Projects extends Component {
           </Carousel>
           <p>test test test</p>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }

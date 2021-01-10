bc7e9c6b1505b89591ac31541b220a71d6f24f34
Kevin Castillo <kevincastillo318@gmail.com>
2021-01-10 11:09:11 -0500

    Updates

---
+++ precache-manifest.09727bfb4eece2653a85c0fee6970d14.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e766ce2f5bab5a22b7a61bad43ab74b",
+    "url": "/KJCastillo/index.html"
+  },
+  {
+    "revision": "00203d6c12d120d4abfd",
+    "url": "/KJCastillo/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "35c9f8215ea51912155b",
+    "url": "/KJCastillo/static/css/main.3e6097ee.chunk.css"
+  },
+  {
+    "revision": "00203d6c12d120d4abfd",
+    "url": "/KJCastillo/static/js/2.65f745a7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/KJCastillo/static/js/2.65f745a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c9f8215ea51912155b",
+    "url": "/KJCastillo/static/js/main.4db04d8e.chunk.js"
+  },
+  {
+    "revision": "31b3f43cb4588e7af364",
+    "url": "/KJCastillo/static/js/runtime-main.2b5c2728.js"
+  },
+  {
+    "revision": "7e533c7392b25594d50f08ac16d827f2",
+    "url": "/KJCastillo/static/media/MiamiTech.7e533c73.png"
+  },
+  {
+    "revision": "b780ecf0272864746e31f791362ef34b",
+    "url": "/KJCastillo/static/media/MiamiTechHome.b780ecf0.png"
+  },
+  {
+    "revision": "0ad3da4925a6d59f701f54921423fb28",
+    "url": "/KJCastillo/static/media/MiamiTechJobs.0ad3da49.png"
+  },
+  {
+    "revision": "7290b88c625b724958feefad58a147f0",
+    "url": "/KJCastillo/static/media/backendLogo.7290b88c.png"
+  },
+  {
+    "revision": "732039ff4e1a593a970e01072b04e597",
+    "url": "/KJCastillo/static/media/bjj.732039ff.png"
+  },
+  {
+    "revision": "fc0271ed9408a4c0eb23e8561a8f6df0",
+    "url": "/KJCastillo/static/media/cart.fc0271ed.png"
+  },
+  {
+    "revision": "e9ef4b052ee5f8986487894fbd5d9d0d",
+    "url": "/KJCastillo/static/media/frontendLogo.e9ef4b05.png"
+  },
+  {
+    "revision": "a9278ceb66a0462fb885f45a054bb8ad",
+    "url": "/KJCastillo/static/media/iceland.a9278ceb.png"
+  },
+  {
+    "revision": "dd8e75765c98fb0c36755226074f7913",
+    "url": "/KJCastillo/static/media/libertyMallRegister.dd8e7576.png"
+  },
+  {
+    "revision": "323d553d1cd5d5206d7a16126475fcc5",
+    "url": "/KJCastillo/static/media/libertyMallSearch.323d553d.png"
+  },
+  {
+    "revision": "70192b7ecbfd60ece94437bd023a5695",
+    "url": "/KJCastillo/static/media/loginModal.70192b7e.png"
+  },
+  {
+    "revision": "c657815612d3a562d5fae5019e91b834",
+    "url": "/KJCastillo/static/media/movies2API.c6578156.png"
+  },
+  {
+    "revision": "81721d0e4cba0d47426fc19c5fd50fca",
+    "url": "/KJCastillo/static/media/moviesAPI.81721d0e.png"
+  },
+  {
+    "revision": "0d119072b968703dc21198d3e89cf3be",
+    "url": "/KJCastillo/static/media/moviesLogo.0d119072.png"
+  },
+  {
+    "revision": "d70b7a3b58139fabcd290c905afe3034",
+    "url": "/KJCastillo/static/media/nature.d70b7a3b.png"
+  },
+  {
+    "revision": "bb4a61c26ec844e024f0342714213d59",
+    "url": "/KJCastillo/static/media/otherLogo.bb4a61c2.png"
+  },
+  {
+    "revision": "3072c70e0a61f2f24a85232a30d85059",
+    "url": "/KJCastillo/static/media/profile.3072c70e.png"
+  },
+  {
+    "revision": "507ed0da5beefee094aeac5168fba383",
+    "url": "/KJCastillo/static/media/searchAPI.507ed0da.png"
+  }
+]);

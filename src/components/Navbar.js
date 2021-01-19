5c518030d408f5203f04a986369cc0283e96174d
Kevin Castillo <kevincastillo318@gmail.com>
2021-01-19 12:55:41 -0500

    updated navbar with resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Pdf from "../pdf/Resume.pdf";
 import "../App.css";
 
 class Navbar extends Component {
@@ -22,6 +23,12 @@ class Navbar extends Component {
             </a>
             <a className="nav-item nav-link" href="#contact">
               Contact
+            </a>
+            <a className="nav-item nav-link ml-auto" target = "_blank" rel="noopener noreferrer" href={Pdf}>
+            <img className="resume-icon" alt="resume" src="https://img.icons8.com/nolan/64/open-resume.png"/>
+            </a>
+            <a className="nav-item nav-link" target = "_blank" rel="noopener noreferrer" style={{paddingLeft: "0"}} href="https://www.linkedin.com/in/kevinjcastillo/">
+            <img className="linkedin-icon" alt="linkedin" src="https://img.icons8.com/nolan/64/linkedin.png"/>
             </a>
           </nav>
         </div>

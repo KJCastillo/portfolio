89aa09fd3ea2067fa45cfca18b3b92737b8e515b
Kevin Castillo <kevincastillo318@gmail.com>
2021-01-19 16:38:10 -0500

    working with media queries

--- src/Layout.js
+++ src/Layout.js
@@ -8,13 +8,7 @@ import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import {CircleArrow as ScrollUpButton} from "react-scroll-up-button";
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<Navbar />, div);
-// });
-
 export const Layout = () => {
-  //const basename = process.env.BASENAME || "";
 
   return (
     <div className="d-flex flex-column h-100">

d5b58f4c3f9e356e2efe03d52fc320a693175078
Kevin Castillo <kevincastillo318@gmail.com>
2021-01-21 13:55:01 -0500

    code formatted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,40 +4,67 @@ import Pdf from "../pdf/Resume.pdf";
 import "../App.css";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <div className="Nav">
-                <nav className="navbar navbar-expand-lg">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <Link className="nav-item nav-link" to="/">
-                            Home
+  render() {
+    return (
+      <div className="Nav">
+        <nav className="navbar navbar-expand-lg">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <Link className="nav-item nav-link" to="/">
+              Home
             </Link>
-                        <a className="nav-item nav-link" href="#skills">
-                            Skills
+            <a className="nav-item nav-link" href="#skills">
+              Skills
             </a>
-                        <Link className="nav-item nav-link" to="/projects">
-                            Projects
+            <Link className="nav-item nav-link" to="/projects">
+              Projects
             </Link>
-                        <a className="nav-item nav-link" href="#about">
-                            About
+            <a className="nav-item nav-link" href="#about">
+              About
             </a>
-                        <a className="nav-item nav-link" href="#contact">
-                            Contact
+            <a className="nav-item nav-link" href="#contact">
+              Contact
             </a>
-                        <a className="nav-item nav-link ml-auto" target="_blank" rel="noopener noreferrer" href={Pdf}>
-                            <img className="resume-icon" alt="resume" src="https://img.icons8.com/nolan/64/open-resume.png" />
-                        </a>
-                        <a className="nav-item nav-link" target="_blank" rel="noopener noreferrer" style={{ paddingLeft: "0" }} href="https://www.linkedin.com/in/kevinjcastillo/">
-                            <img className="linkedin-icon" alt="linkedin" src="https://img.icons8.com/nolan/64/linkedin.png" />
-                        </a>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
+            <a
+              className="nav-item nav-link ml-auto"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={Pdf}
+            >
+              <img
+                className="resume-icon"
+                alt="resume"
+                src="https://img.icons8.com/nolan/64/open-resume.png"
+              />
+            </a>
+            <a
+              className="nav-item nav-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ paddingLeft: "0" }}
+              href="https://www.linkedin.com/in/kevinjcastillo/"
+            >
+              <img
+                className="linkedin-icon"
+                alt="linkedin"
+                src="https://img.icons8.com/nolan/64/linkedin.png"
+              />
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;

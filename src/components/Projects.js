0b0e672cded5abe5f2163eb7da2d871be9635097
Kevin Castillo <kevincastillo318@gmail.com>
2021-10-28 12:30:58 -0700

    added toDoList to project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,11 +15,15 @@ import moviesAPI from "../img/moviesAPI.png";
 import movies2API from "../img/movies2API.png";
 import searchAPI from "../img/searchAPI.png";
 import moviesLogo from "../img/moviesLogo.png";
+import toDoList from "../img/toDoList.png";
+import toDoList2 from "../img/toDoList2.png";
+import toDoList3 from "../img/toDoList3.png";
+import listLogo from "../img/listLogo.png";
 
 export const Projects = () => {
   useEffect(() => {
-    Aos.init({disable: 'mobile'});
-    Aos.init({disable: 'tablet'});
+    Aos.init({ disable: "mobile" });
+    Aos.init({ disable: "tablet" });
     Aos.init({ duration: 1000 });
   }, []);
   return (
@@ -27,8 +31,104 @@ export const Projects = () => {
       <div className="text-center projectTitle">
         <h1>Projects</h1>
       </div>
-
-      <div className="row">
+      {/* start of to do list react */}
+      <div className="row pt-4 mb-4">
+        <div className="col-sm pt-4">
+          <div>
+          {/* data-aos="fade-right" */}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://to-do-list-kjcastillo.vercel.app/"
+            >
+              <img src={listLogo} className="rounded" alt="to do list logo" />
+            </a>
+          </div>
+          <div className="card-body">
+            <p className="card-text">
+              To do list website made to help productivity in completing tasks
+              with ease. User is able to create a new task, mark task as
+              complete and delete task when complete. Created on React.js.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="navbar-brand ml-auto"
+              href="https://github.com/KJCastillo/toDoListReact"
+            >
+              <img
+                className="project-icons"
+                alt="github"
+                src="https://img.icons8.com/nolan/64/github.png"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="projectCarousel col-sm pt-4">
+          <div
+            id="carouselExampleControls2"
+            className="carousel slide"
+            data-ride="carousel"
+            data-interval="false"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  src={toDoList}
+                  width="100%"
+                  height="500"
+                  className=""
+                  alt="..."
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src={toDoList2}
+                  width="100%"
+                  height="500"
+                  className=""
+                  alt="..."
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src={toDoList3}
+                  width="100%"
+                  height="500"
+                  className=""
+                  alt="..."
+                />
+              </div>
+            </div>
+            <a
+              className="carousel-control-prev"
+              href="#carouselExampleControls2"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#carouselExampleControls2"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* start of liberty mall */}
+      <div className="row mb-4">
         <div className="projectCarousel col-sm pt-4">
           <div
             id="carouselExampleControls"
@@ -101,13 +201,15 @@ export const Projects = () => {
           </div>
         </div>
         <div className="col-sm text-right pt-4 ">
-          <img src={lmLogo} alt="liberty mall logo" width="300" />
+        <div data-aos="fade-left">
+        <img src={lmLogo} alt="liberty mall logo" width="300" />
+        </div>
           <div className="card-body">
             <p className="card-text">
               An online shop that uses Amazon's API to gather product
               information to sell to the public. Frontend created on a React.js
               framework using HTML, CSS, and JavScript. Backend created on a
-              Flask framework using Python and SQLAlchemy
+              Flask framework using Python and SQLAlchemy.
             </p>
             <a
               target="_blank"
@@ -124,38 +226,38 @@ export const Projects = () => {
           </div>
         </div>
       </div>
-
-      <div className="row pt-4">
+      {/* end of liberty mall */}
+      {/* start of miami works */}
+      <div className="row pt-4 mb-4">
         <div className="col-sm pt-4">
-        <div data-aos="fade-right">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://miamitech.works/"
-          >
-            <img src={mwLogo} alt="miami tech works logo" />
-          </a>
+          <div data-aos="fade-right">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://miamitech.works/"
+            >
+              <img src={mwLogo} alt="miami tech works logo" />
+            </a>
           </div>
           <div className="card-body">
             <p className="card-text">
               A portal created by the coalition of Miami's business, education
               and government leaders combining resources to help local citizens.
               Volunteered to design Jobs Section for desktop and mobile view
-              using Gatsby.js
+              using Gatsby.js.
             </p>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className="navbar-brand ml-auto"
-                href="https://github.com/miamiworks/web"
-              >
-                <img
-                  className="project-icons"
-                  alt="github"
-                  src="https://img.icons8.com/nolan/64/github.png"
-                />
-              </a>
-            
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="navbar-brand ml-auto"
+              href="https://github.com/miamiworks/web"
+            >
+              <img
+                className="project-icons"
+                alt="github"
+                src="https://img.icons8.com/nolan/64/github.png"
+              />
+            </a>
           </div>
         </div>
         <div className="projectCarousel col-sm pt-4">
@@ -221,8 +323,9 @@ export const Projects = () => {
           </div>
         </div>
       </div>
-
-      <div className="row pt-4">
+      {/* end of miami works */}
+      {/* start of moviesAPI */}
+      <div className="row pt-4 mb-4">
         <div className="projectCarousel col-sm pt-4">
           <div
             id="carouselExampleControls3"
@@ -322,6 +425,7 @@ export const Projects = () => {
           </div>
         </div>
       </div>
+      {/* end of moviesAPI */}
     </div>
   );
 };

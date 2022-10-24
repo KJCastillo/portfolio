3a5726ff915bf9d451e7077bf0457645fd174328
Kevin Castillo <kevincastillo318@gmail.com>
2022-10-23 17:30:25 -0700

    skills rebuilding v1

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../App.css";
-import '../css/Skills.css'
+import "../css/Skills.css";
 import profile from "../img/profile.png";
 //import frontendLogo from "../img/frontendLogo.png";
 // import backendLogo from "../img/backendLogo.png";
@@ -10,42 +10,57 @@ class Skills extends Component {
   render() {
     return (
       <div className="Skills container-fluid" id="skills">
-        <div className="text-center">
+        <div className="title text-center pb-3">
           <h1>Skills</h1>
         </div>
-        <div className="skills-left">
-          <img src={profile} alt="profile-image" width="400px" height="460px" />
-        </div>
-        <div className="skills-right">
-          <div className="skills-section">
-            <ul className="skills-list">
-              <li className="skill">HTML</li>
-              <li className="skill">CSS</li>
-              <li className="skill">Bootstrap</li>
-              <li className="skill">JavaScript</li>
-              <li className="skill">jQuery</li>
-              <li className="skill">React</li>
-              <li className="skill">Python</li>
-              <li className="skill">Node.js</li>
-              <li className="skill">NPM</li>
-              <li className="skill">REST API</li>
-              <li className="skill">Object Oriented Programming</li>
-              <li className="skill">Databases</li>
-              <li className="skill">Firebase</li>
-              <li className="skill">Adobe Photoshop</li>
-              <li className="skill">Github</li>
-            </ul>
-          </div>
-          <div className="small-bio">
-            <h1>I love building stuff</h1>
-            <p>
-              Whether it was Legos or airplane models, I always loved using my
-              brain and hands to create something from scratch. And after coming
-              across coding, I was all in! Being able to problem solve and
-              create responsive and dynamic web sites has become so much more
-              than a career path, but a passion.
-            </p>
-            <button className="skill-button">Kep reading my bio</button>
+
+        <div className="flex-container">
+          <div className="row justify-content-center">
+            <div className="col-sm-10">
+              <div className="row">
+                <div className="img-div col-4">
+                  <img
+                    src={profile}
+                    alt="profile-pic"
+                    width="350px"
+                    height="360px"
+                  />
+                </div>
+                <div className="col-8">
+                  <div className="skills-section">
+                    <ul className="skills-list wrap">
+                      <li className="skill">HTML</li>
+                      <li className="skill">CSS</li>
+                      <li className="skill">Bootstrap</li>
+                      <li className="skill">JavaScript</li>
+                      <li className="skill">jQuery</li>
+                      <li className="skill">React</li>
+                      <li className="skill">Python</li>
+                      <li className="skill">Node.js</li>
+                      <li className="skill">NPM</li>
+                      <li className="skill">REST API</li>
+                      <li className="skill">Object Oriented Programming</li>
+                      <li className="skill">Databases</li>
+                      <li className="skill">Firebase</li>
+                      <li className="skill">Adobe Photoshop</li>
+                      <li className="skill">Github</li>
+                    </ul>
+                  </div>
+                  <div className="small-bio">
+                    <h2 className="title pt-5 pb-3">I love building stuff</h2>
+                    <p>
+                      Whether it was Legos or airplane models, I always loved
+                      using my brain and hands to create something from scratch.
+                      And after coming across coding, I was all in! Being able
+                      to problem solve and create responsive and dynamic web
+                      sites has become so much more than a career path, but a
+                      passion.
+                    </p>
+                    <button className="bio-button">Keep reading my bio</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         {/* <div className="card-deck justify-content-center pt-4">
